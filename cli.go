76ad98eb2c3a3856395f3ae88ac702120f50a0d8
Arrafly Dori <arraflydori@gmail.com>
2026-06-03 21:10:42 +0700

    feat: setup cli

---
+++ cli.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/nichidori/redm/internal/rest"
+)
+
+type state struct {
+	client *rest.Client
+}
+
+type Command struct {
+	Name        string
+	Description string
+	Setup       func(fs *flag.FlagSet, s *state) func() error
+}
+
+type CLI struct {
+	Name     string
+	State    *state
+	Commands map[string]Command
+}
+
+func (c *CLI) Register(cmd Command) {
+	if c.Commands == nil {
+		c.Commands = make(map[string]Command)
+	}
+	c.Commands[cmd.Name] = cmd
+}
+
+func (c *CLI) Execute(args []string) error {
+	if len(args) < 1 {
+		c.PrintGlobalHelp()
+		return nil
+	}
+
+	cmdName := args[0]
+	cmd, exists := c.Commands[cmdName]
+	if !exists {
+		return fmt.Errorf("unknown command: %s", cmdName)
+	}
+
+	fs := flag.NewFlagSet(cmd.Name, flag.ExitOnError)
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s %s [options]\n\n", c.Name, cmd.Name)
+		fmt.Fprintf(os.Stderr, "%s\n\nOptions:\n", cmd.Description)
+		fs.PrintDefaults()
+	}
+
+	run := cmd.Setup(fs, c.State)
+
+	if err := fs.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	return run()
+}
+
+func (c *CLI) PrintGlobalHelp() {
+	fmt.Printf("Usage: %s <command> [options]\n", c.Name)
+	fmt.Println("\nAvailable Commands:")
+	for _, cmd := range c.Commands {
+		fmt.Printf("  %-15s %s\n", cmd.Name, cmd.Description)
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/nichidori/redm/internal/rest"
+)
+
+func main() {
+	c := rest.NewClient(
+		"http://localhost:3000",
+		"e7315305dc0f377b1f5d0a37ac353e47436a5b2a",
+	)
+
+	cli := &CLI{
+		Name: "redm",
+		State: &state{
+			client: c,
+		},
+	}
+
+	if err := cli.Execute(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
